0658c72ff7befa250deac45e751a58c8b3ce144c
alsawave <averagedayincalifornia.contact@protonmail.com>
2026-05-16 05:33:35 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 import { createIcons, Search, MapPin, ArrowRight, Menu, X, Clock, Smartphone, ChevronRight, Phone, Mail, Facebook, Instagram, XCircle, Check, TrainFront, Calendar, Info, ArrowLeft, CreditCard, RefreshCw, ShieldCheck, Wifi, Lock, CheckCircle, Award } from 'lucide';
 
 // DOM Elements
-const newsContainer = document.getElementById('news-container');
-const newsModal = document.getElementById('news-modal');
-const closeNewsModal = document.getElementById('close-news-modal');
-const closeNewsModalBtn = document.getElementById('close-news-modal-btn');
-const showAllNewsBtn = document.getElementById('show-all-news');
-const searchForm = document.getElementById('search-form');
+const newsContainer = document.getElementById("news-container");
+const newsModal = document.getElementById("news-modal");
+const closeNewsModal = document.getElementById("close-news-modal");
+const closeNewsModalBtn = document.getElementById("close-news-modal-btn");
+const showAllNewsBtn = document.getElementById("show-all-news");
+const searchForm = document.getElementById("search-form");
 
 const ICON_CONFIG = {
     icons: {
