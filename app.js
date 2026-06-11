a6c7a2952200792072d0d443cd7eafa82d630a93
Piotr-Trans <254098274+alsawave@users.noreply.github.com>
2026-06-11 19:31:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -140,6 +140,17 @@ function wyszukiwarka(inputId, suggestionsId) {
             suggestions.classList.add('hidden');
             return;
         }
+        if (sugestie1.length === 0) {
+        suggestions.classList.add('hidden');
+        const prev = input.value;
+        input.value = "Nie ma takiej stacji!";
+        setTimeout(() => {
+        input.value = prev;
+        input.focus();
+        input.setSelectionRange(prev.length, prev.length);
+        }, 1200);
+        return;
+        }
 
         suggestions.innerHTML = sugestie1.map(station => `
             <div class="suggestion-item p-3 border-b last:border-0 flex items-center gap-3" data-value="${station.name}">
